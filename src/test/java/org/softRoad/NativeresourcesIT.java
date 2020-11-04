8c4f7d2c51b8c44b683a38cd4812a04e0eb0fe48
Navid <navid@localhost.localdomain>
2020-11-04 12:25:57 -0500

    Initialize project

---
+++ src/test/java/org/softRoad/NativeresourcesIT.java
@@ -0,0 +1,9 @@
+package org.softRoad;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeresourcesIT extends resourcesTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ src/test/java/org/softRoad/resourcesTest.java
@@ -0,0 +1,21 @@
+package org.softRoad;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class resourcesTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/softRoad")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
