4fe35ec2866ce725c449d52812e403d1a8a9b08b
vahid-dev <vahidgh.vgh@gmail.com>
2020-12-28 03:17:29 +0330

    declare functions of CategoryService

---
+++ src/main/java/org/softRoad/services/CategoryService.java
@@ -0,0 +1,43 @@
+package org.softRoad.services;
+
+import org.softRoad.models.Category;
+import org.softRoad.models.Procedure;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@ApplicationScoped
+
+public class CategoryService extends CrudService<Category> {
+
+    @Inject
+    EntityManager entityManager;
+
+    public CategoryService() {
+        super(Category.class);
+    }
+
+    @Transactional
+    public List<Category> getCategoriesOfProcedure(Procedure procedure) {
+
+    }
+
+    @Transactional
+    public void setCategoryForProcedure(Procedure procedure) {
+
+    }
+
+    @Transactional
+    public void removeCategoryFromProcedure(Procedure procedure) {
+
+    }
+
+    @Transactional
+    public List<Procedure> getProceduresOfCategory(Category category) {
+
+    }
+
+}
