cc06156d9073f69b2101bae1a1ceaea31c258d96
vahid-dev <vahidgh.vgh@gmail.com>
2020-12-28 03:17:52 +0330

    declare functions of StepService

---
+++ src/main/java/org/softRoad/services/StepService.java
@@ -0,0 +1,43 @@
+package org.softRoad.services;
+
+import org.softRoad.models.Procedure;
+import org.softRoad.models.Step;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+public class StepService extends CrudService<Step> {
+    @Inject
+    EntityManager entityManager;
+
+    public StepService() {
+        super(Step.class);
+    }
+
+    @Transactional
+    public void setStepForProcedure(Step step, Integer pid) {
+        Procedure procedure = Procedure.findById(pid);
+
+    }
+
+    @Transactional
+    public void setStepsForProcedure(List<Step> steps, Integer pid) {
+
+    }
+
+    @Transactional
+    public void removeStepFromProcedure(Integer sid, Integer pid) {
+        Procedure procedure = Procedure.findById(pid);
+        Step step = Step.findById(sid);
+
+    }
+
+    @Transactional
+    public List<Step> getStepsOfProcedure(Integer pid) {
+        Procedure procedure = Procedure.findById(pid);
+
+    }
+
+}
