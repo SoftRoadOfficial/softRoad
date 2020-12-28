84d73b083f575e07ab5aede9de833ca36f3f370d
Navid-fkh <mojtaba.farokhi99@gmail.com>
2020-12-28 20:40:24 +0330

    Update travis command Remove redundant test files

--- src/test/java/org/softRoad/NativeresourcesIT.java
+++
@@ -1,9 +0,0 @@
-package org.softRoad;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeresourcesIT extends resourcesTest {
-
-    // Execute the same tests but in native mode.
-}
