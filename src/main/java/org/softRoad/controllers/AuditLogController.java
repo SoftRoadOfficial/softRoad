a90ff8d6709ef13d9065405cea4857e9873a2d1d
mahdi <mhf1377@gmail.com>
2020-12-29 19:07:01 +0330

    Initialize AuditLog classes

--- src/main/java/org/softRoad/controllers/AuditLogController.java
+++ src/main/java/org/softRoad/controllers/AuditLogController.java
@@ -1,4 +1,40 @@
 package org.softRoad.controllers;
 
+import org.softRoad.models.AuditLog;
+import org.softRoad.services.AuditLogService;
+
+import javax.enterprise.context.RequestScoped;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/auditLog")
+@RequestScoped
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class AuditLogController {
+
+    private final AuditLogService auditLogService;
+
+    public AuditLogController(AuditLogService auditLogService) {
+        this.auditLogService = auditLogService;
+    }
+
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("create")
+    public Response create(@Valid AuditLog auditLog) {
+        return auditLogService.create(auditLog);
+    }
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public AuditLog get(@PathParam("id") Integer id) {
+        return auditLogService.get(id);
+    }
+
 }
--- src/main/java/org/softRoad/services/AuditLogService.java
+++ src/main/java/org/softRoad/services/AuditLogService.java
@@ -1,4 +1,19 @@
 package org.softRoad.services;
 
-public class AuditLogService {
+import org.softRoad.models.AuditLog;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+@ApplicationScoped
+public class AuditLogService extends CrudService<AuditLog> {
+
+    @Inject
+    EntityManager entityManager;
+
+    public AuditLogService() {
+        super(AuditLog.class);
+    }
+
 }
