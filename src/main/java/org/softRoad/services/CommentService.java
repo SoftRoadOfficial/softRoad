61c16ccc65fe9e83ed3b6bbdaf0b0c02f86d4e12
mahdi <mhf1377@gmail.com>
2021-01-08 23:14:27 +0330

    Remove buggy function

--- src/main/java/org/softRoad/services/CommentService.java
+++ src/main/java/org/softRoad/services/CommentService.java
@@ -3,18 +3,14 @@ package org.softRoad.services;
 import org.softRoad.exception.InvalidDataException;
 import org.softRoad.models.Comment;
 import org.softRoad.models.User;
-import org.softRoad.models.query.HqlQuery;
-import org.softRoad.models.query.QueryUtils;
 import org.softRoad.models.query.SearchCriteria;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import javax.validation.constraints.NotNull;
+import java.util.Collections;
 import java.util.List;
-
-import static org.softRoad.models.Tables.USERS;
 
 
 @ApplicationScoped
@@ -32,14 +28,15 @@ public class CommentService extends CrudService<Comment> {
             throw new InvalidDataException("Invalid user");
 
         // TODO: search in all comments to find the ones with the user == comment.user_id
-        Query q = QueryUtils.nativeQuery(entityManager, User.class)
-                .baseQuery(new HqlQuery("select u.* from %s left join %s as u on u.%s=%s",
-                        USER_COMMENTS, USERS, User.ID, UserComment.fields(UserComment.USER_ID)))
-                .addFilter(new HqlQuery("%s=:idd", UserComment.COMMENTS_ID).setParameter("idd", comment.id))
-                .searchCriteria(searchCriteria)
-                .build();
+//        Query q = QueryUtils.nativeQuery(entityManager, User.class)
+//                .baseQuery(new HqlQuery("select u.* from %s left join %s as u on u.%s=%s",
+//                        USER_COMMENTS, USERS, User.ID, UserComment.fields(UserComment.USER_ID)))
+//                .addFilter(new HqlQuery("%s=:idd", UserComment.COMMENTS_ID).setParameter("idd", comment.id))
+//                .searchCriteria(searchCriteria)
+//                .build();
 
-        return q.getResultList();
+//        return q.getResultList();
+        return Collections.emptyList();
     }
 
 }
